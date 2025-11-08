a55d9a3a7d8292cb3de035e7703cd869613ec766
ValerioTav <vt1u24@soton.ac.uk>
2025-11-08 23:35:25 +0000

    chatPage and homePage frameworks
    created the frameworks for the chatPage and homePage, to be revisited tomorrow.

--- pages/chatPage.py
+++ pages/chatPage.py
@@ -4,163 +4,87 @@ import pandas as pd
 import numpy as np
 
 # title
-st.title("Example Title")
+st.title("Chatbot Page")
 
-# chatbot page elements
-prompt = st.chat_input("Say something")
-if prompt:
-    newmessage = st.chat_message("user")
-    newmessage.write(prompt)
-
+# messages to pre-load (would be fetched from database)
 messages=[
-    {"sender":"assistant","text":"hello!"},
-    {"sender":"user","text":"response"},
-]
-
-for m in messages:
-    message = st.chat_message(m["sender"])
-    message.write(m["text"])
-
-# calendar
-
-timeEvents=[
     {
-        # "allDay":True,
-        "title":"Event 1",
-        "start":"2025-11-03",
-        "end":"2025-11-03",
-        "backgroundColor":"#FF6C6C",
-        "borderColor":"#FF6C6C",
-        "resourceId":"a",
+        "sender":"user",
+        "text":"hello! generic query"
     },
     {
-        "allDay":True,
-        "title":"Event 3",
-        "start":"2025-11-03",
-        "end":"2025-11-03",
-        "backgroundColor":"#FF6C6C",
-        "borderColor":"#FF6C6C",
-        "resourceId":"a",
-    },
-]
-
-def exampleFunc():
-    st.rerun()
-
-st.set_page_config(page_title="Demo for streamlit-calendar", page_icon="📆")
-
-events = [
-    {
-        "title": "Event 1",
-        "color": "#FF6C6C",
-        "start": "2023-07-03",
-        "end": "2023-07-05",
-        "resourceId": 1,
+        "sender":"assistant",
+        "text":"response"
     },
     {
-        "title": "Event 2",
-        "color": "#FFBD45",
-        "start": "2023-07-01",
-        "end": "2023-07-10",
-        "resourceId": 2,
+        "sender":"user",
+        "text":"hello! generic query"
+    },
+    {
+        "sender":"assistant",
+        "text":"response"
+    },
+    {
+        "sender":"user",
+        "text":"hello! generic query"
+    },
+    {
+        "sender":"assistant",
+        "text":"response"
+    },
+    {
+        "sender":"user",
+        "text":"hello! generic query"
+    },
+    {
+        "sender":"assistant",
+        "text":"response"
+    },
+    {
+        "sender":"user",
+        "text":"hello! generic query"
+    },
+    {
+        "sender":"assistant",
+        "text":"response"
     },
 ]
 
-calendar_resources = [
-    {"id": 1, "building": "Building A", "title": "Room A"},
-    {"id": 2, "building": "Building A", "title": "Room B"},
-]
+# button to go back to home page
 
-calendar_options = {
-    "editable": "true",
-    "navLinks": "true",
-    "resources": calendar_resources,
-    "selectable": "true",
-    "headerToolbar": {
-        "left": "today prev,next",
-        "center": "title",
-        "right": "dayGridDay,dayGridWeek,dayGridMonth",
-    },
-    "initialDate": "2023-07-01",
-    "initialView": "dayGridMonth",
-}
+btnContainer = st.container(horizontal=True,horizontal_alignment='distribute')
 
-call_event=[
-    'eventClick'
-]
-
-state = calendar(
-    events=events,
-    options=calendar_options,
-    callbacks=call_event,
-    custom_css="""
-    .fc-event-past {
-        opacity: 0.8;
-    }
-    .fc-event-time {
-        font-style: italic;
-    }
-    .fc-event-title {
-        font-weight: 700;
-    }
-    .fc-toolbar-title {
-        font-size: 2rem;
-    }
-    """,
-    key="timegrid",
+returnButton = btnContainer.button(
+    "Return to Home",
+    icon=":material/arrow_back_ios:",
 )
 
-beenClicked=False
-# Handle calendar interactions
-if state.get("eventClick"):
-    # Get clicked event details
-    clicked_event = state["eventClick"]["event"]
-    event_title = clicked_event["title"]
-    event_start = clicked_event["start"]
-
-    st.session_state.current_event=clicked_event
-    st.switch_page("pages/formView.py")
-
-# home page elements
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
-
-# Notify the reader that the data was successfully loaded.
-data_load_state.text("")
-
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
+with btnContainer.popover("Additional Resources",icon=":material/phone:"):
+    st.markdown("resource 1")
+    st.markdown("resource 1")
+    st.markdown("resource 1")
 
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
-st.line_chart(hist_values)
+if returnButton:
+    st.switch_page("pages/homePage.py")
 
-hour_to_filter = st.slider('hour',0,23,17)
+# chat input element
+prompt = st.chat_input("Say something")
+if prompt:
+    # newmessage = st.chat_message("user")
+    # newmessage.write(prompt)
 
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-st.map(filtered_data)
+    # replace this with a push to the database,
+    # and add the code that triggers LLM to create a response
+    messages.append({
+        "sender":"user",
+        "text":prompt,
+    })
 
-enable = st.checkbox("Enable camera")
-picture = st.camera_input("Take a picture", disabled=not enable)
+# streamlit recommends avoiding scrolling containers of >500px for mobile phone compatibility reasons
+chatContainer = st.container(height=800)
 
-# if picture:
-#     st.image(picture)
+# generates element for each message
+for m in messages:
+    message = chatContainer.chat_message(m["sender"])
+    message.write(m["text"])
--- pages/homePage.py
+++ pages/homePage.py
@@ -2,5 +2,36 @@ import streamlit as st
 from streamlit_calendar import calendar
 import pandas as pd
 import numpy as np
+# from datetime import date
 
-st.write(st.session_state)
+# home page elements
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+    
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+
+# Load 10,000 rows of data into the dataframe.
+data = load_data(10000)
+
+# Notify the reader that the data was successfully loaded.
+data_load_state.text("")
+
+st.subheader('Mood Graph:')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.line_chart(hist_values,x_label="Time",y_label="Mood",color="#64C46C")
+
+# update to track current day - update dependencies
+filtered_data = data[data[DATE_COLUMN].dt.day==9]
+st.subheader("Map of your activity today:")
+st.map(filtered_data)
